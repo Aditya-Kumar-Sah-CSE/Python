dcb629da74eac1fe52eab6e48109147e7aceb713
Aditya Kumar Sah <adityakumarsah8709@gmail.com>
2025-06-11 23:55:22 +0530

    problem 02

---
+++ problem_01.py
@@ -0,0 +1,48 @@
+#1. Write a program to store seven fruits in a list entered by the user?
+
+# fruits = []
+ 
+# f1 = input("Enter Fruit name : ")
+# fruits.append(f1)
+
+# f2 = input("Enter Fruit name : ")
+# fruits.append(f2)
+
+# f3 = input("Enter Fruit name : ")
+# fruits.append(f3)
+# f4 = input("Enter Fruit name : ")
+# fruits.append(f4)
+# f5 = input("Enter Fruit name : ")
+# fruits.append(f5)
+# f6 = input("Enter Fruit name : ")
+# fruits.append(f6)
+# f7 = input("Enter Fruit name : ")
+# fruits.append(f7)
+# print(fruits)
+
+
+# 2. Write a program to accept marks of 6 students and display them in a sorted manner.
+
+marks = []
+ 
+m1 =int(input("Enter marks  : "))
+marks.append(m1)
+
+m2 =int(input("Enter marks  : "))
+marks.append(m2)
+
+m3 =int(input("Enter marks  : "))
+marks.append(m3)
+m4 =int(input("Enter marks  : "))
+marks.append(m4)
+m5 =int(input("Enter marks  : "))
+marks.append(m5)
+
+m6 =int(input("Enter marks  : "))
+marks.append(m6)
+
+# to sort
+marks.sort()
+
+print(marks)
+
