ed320bf3824b85cea711fe56d9dcf780f758b8de
Aditya Kumar Sah <adityakumarsah8709@gmail.com>
2025-06-12 01:48:31 +0530

    Python PS-5 p1

--- problem_01.py
+++ problem_01.py
@@ -46,3 +46,49 @@ marks.sort()
 
 print(marks)
 
+# 3. Check that a tuple type cannot be changed in python.
+ 
+touple1 = ( 'hii', 121,"true")
+
+#touple1[1]=128
+# TypeError: 'tuple' object does not support item assignment
+
+print(touple1)
+
+# Write a program to sum a list with 4 numbers.
+
+list=[3,5,6,1]
+print(sum(list))
+
+# Write a program to count the number of zeros in the following tuple:
+a = (7, 0, 8, 0, 0, 9)
+n=a.count(0)
+print(n)
+
+
+# chapter 5 - dictonary 
+a={
+    "name": "harry",
+"from": "india",
+"marks": [92,98,96]
+}
+print(a.items())
+
+print(a.keys())
+
+print(a.values())
+
+# dict_items([('name', 'harry'), ('from', 'india'), ('marks', [92, 98, 96])])
+# dict_keys(['name', 'from', 'marks'])
+# dict_values(['harry', 'india', [92, 98, 96]])
+
+a.update({"Percent": "96%",'marks' :[90,98,96]})
+print(a)
+
+# sets
+
+# practice set 5
+ 
+ 
+# 1. Write a program to create a dictionary of Hindi words with values as their English 
+# translation. Provide user with an option to look it up! 
